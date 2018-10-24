8f93b932c08ac9667e509d543a3d1856155ca29b
Benjamin Bar-Gera <benibargera@gmail.com>
2018-10-24 10:12:05 +0200

    Finalize function

--- table_of_contents.rb
+++ table_of_contents.rb
@@ -8,6 +8,6 @@ puts title.center lineWidth
 info = ["1","Numbers","1","2","Letters","72","3","Variables","118"]
 i = 0
 while i < info.length
-  puts info[i].ljust(lineWidth/2) + pages[i].rjust(lineWidth/2)
-  i = i + 1
+  puts ("Chapter " + info[i] + ":  " + info[i+1]).ljust(lineWidth/2) + ("page " + info[i+2]).rjust(lineWidth/2)
+  i = i + 3
 end
