49b867010565f62b1792c6a2b7a423b5b990a16b
Benjamin Bar-Gera <benibargera@gmail.com>
2018-10-24 10:03:07 +0200

    First commit

--- table_of_contents.rb
+++ table_of_contents.rb
@@ -3,11 +3,11 @@ title = "Table of Contents"
 
 puts title.center lineWidth
 
-chapters = ["Chapter 1:  Numbers","Chapter 2:  Letters","Chapter 3:  Variables"]
-pages = ["page 1", "page 72", "page 118"]
+# info = [Chapter Number, Chapter Name, Page Number]
 
+info = ["1","Numbers","1","2","Letters","72","3","Variables","118"]
 i = 0
-while i < chapters.length
-  puts chapters[i].ljust(lineWidth/2) + pages[i].rjust(lineWidth/2)
+while i < info.length
+  puts info[i].ljust(lineWidth/2) + pages[i].rjust(lineWidth/2)
   i = i + 1
 end
